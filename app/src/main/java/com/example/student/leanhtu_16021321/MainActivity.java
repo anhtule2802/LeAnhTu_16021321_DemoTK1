51023b29ab471b60530b2145b6662b6456bb4aa2
AnhTuIT <anhtule2802@gmail.com>
2019-09-09 14:25:10 +0700

    Second Commit

--- app/src/main/java/com/example/student/leanhtu_16021321/MainActivity.java
+++ app/src/main/java/com/example/student/leanhtu_16021321/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.leanhtu_16021321;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,15 +22,31 @@ public class MainActivity extends AppCompatActivity {
         btnDN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(cb.isChecked())
-                {
-                    Toast.makeText(MainActivity.this,"Chào mừng bạn đăng nhập hệ thống, thông tin của bạn đã được lưu",Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Toast.makeText(MainActivity.this,"Chào mừng bạn đăng nhập hệ thống, thông tin của bạn không được lưu",Toast.LENGTH_LONG).show();
+                if (cb.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đăng nhập hệ thống, thông tin của bạn đã được lưu", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đăng nhập hệ thống, thông tin của bạn không được lưu", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    private void alertDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Thông Báo");
+        alert.setMessage("Bạn có muốn thoát không ?");
+        alert.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alert.setPositiveButton("Không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alert.show();
     }
 }
