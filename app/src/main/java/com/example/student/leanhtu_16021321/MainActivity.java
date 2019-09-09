fc37313e57e1693d549676e31524851c8713459f
AnhTuIT <anhtule2802@gmail.com>
2019-09-09 14:08:46 +0700

    first commit

---
+++ app/src/main/java/com/example/student/leanhtu_16021321/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.student.leanhtu_16021321;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox cb;
+    Button btnDN;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnDN = findViewById(R.id.btnDN);
+        cb = findViewById(R.id.checkBox);
+
+        btnDN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cb.isChecked())
+                {
+                    Toast.makeText(MainActivity.this,"Chào mừng bạn đăng nhập hệ thống, thông tin của bạn đã được lưu",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Chào mừng bạn đăng nhập hệ thống, thông tin của bạn không được lưu",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
